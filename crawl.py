f81714eb7d50ac445948cf875b4d98bc48f0dec6
h0nda <41300123+h0nde@users.noreply.github.com>
2021-10-29 16:40:41 +0200

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,8 +1,6 @@
 if __name__ == "__main__":
     from crawler.controllers import Controller
     from crawler.arguments import parse_args
-    import time
 
     controller = Controller(parse_args())
     controller.start()
-    time.sleep(111111)
